ab650cabf4da3071d97a61fc5b287673df39c1f3
YizhakTime <ydcohen@mail.csuchico.edu>
2025-03-04 12:43:24 -0800

    Added initial GA

---
+++ soccer_ga.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+print("Hello")
+
+def genetic_algorithm():
+    pass
+
+def mutate(chromosome1 : str, chromsome2 : str) -> str:
+    return ""
+
+def crossover(chromosome1 : str, chromsome2 : str) -> str:
+    return ""
+
+def eval_fitness(chromosome1 : str) -> str:
+    return ""
