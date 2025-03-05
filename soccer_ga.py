66773292ef74c5b2f3c95295902b7faf4644d2ba
YizhakTime <ydcohen@mail.csuchico.edu>
2025-03-04 18:26:38 -0800

    Add soccer data

--- soccer_ga.py
+++ soccer_ga.py
@@ -3,8 +3,20 @@ import pandas as pd
 
 print("Hello")
 
-def genetic_algorithm():
-    pass
+time = 0
+
+def generate_random_pop() -> list[str]:
+    return []
+
+def genetic_algorithm() -> str:
+    pop = generate_random_pop()
+    while len(pop) > 0:
+        pass
+    return ""
+
+def select() -> tuple[str, str]:
+    c1, c2 = "", ""
+    return c1, c2
 
 def mutate(chromosome1 : str, chromsome2 : str) -> str:
     return ""
